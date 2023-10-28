f9978cf966a0f462eb2b471b00e4565a35cff059
oxf <radiopineapple@gmail.com>
2023-10-28 19:19:30 +0300

    first

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public byte[] Password { get; set; }
+        public byte[] Salt { get; set; }
+    }
+}
---
+++ Models/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class UserDbContext: DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
